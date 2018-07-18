a355b0ad9d7c702b1763cc64f0ce7304e3f59f0b
Tao <werewaswas@live.com>
2018-07-18 06:05:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
   attr_accessor :artist, :title
   
-  def initialize(artist, title)
-    @artist=Artist.new(artist)
-    @title=title    
-  end
+  # def initialize(artist, title)
+  #   @artist=Artist.new(artist)
+  #   @title=title    
+  # end
   
 end
